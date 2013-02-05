dd83933cc5bb409428a77ad59113eaec1b5936fa
Ryan Castillo <rmcastil@gmail.com>
2013-02-05 14:57:55 -0500

    User can specify another config file for their Amazon credentials

--- lib/credential.rb
+++ lib/credential.rb
@@ -1,10 +1,9 @@
 class Credential
   def initialize(config_file)
-    @config ||= begin
-      unless File.exists?(config_file)
-        abort "Pairhost: No ~/.pairhost/config.yml found. First run 'pairhost init'."
-      end
-      config = YAML.load_file(config_file)
+    @config = load_yaml_file(config_file)
+
+    unless aws_credential_file.nil?
+      @config = load_yaml_file(File.expand_path(aws_credential_file))
     end
   end
 
@@ -14,5 +13,19 @@ class Credential
 
   def access_key_id
     @config['aws_access_key_id']
+  end
+
+  def aws_credential_file
+    @config['aws_credential_file']
+  end
+
+  private
+
+  def load_yaml_file(file)
+    unless File.exists?(file)
+      abort "#{file} not found. First run 'pairhost init' or confirm file exists."
+    else
+      YAML.load_file(file)
+    end
   end
 end
--- spec/credential_spec.rb
+++ spec/credential_spec.rb
@@ -16,6 +16,11 @@ describe Credential do
   end
 
   context "given a config file for credentials within pairhost config file" do
-    it 'should return an access key'
+    let(:config_file) { File.expand_path('../data/config_with_aws_path.yml', __FILE__) }
+
+    it 'should return an access key' do
+      credential = Credential.new(config_file)
+      credential.secret_access_key.should == "YOUR_AWS_SECRET_ACCESS_KEY"
+    end
   end
 end
